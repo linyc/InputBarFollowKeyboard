b6040c773bac8dc89cc9aa480d139246b1ef148e
linycc <linycc@sina.cn>
2015-01-26 15:21:58 +0800

    first commit

---
+++ InputBarFollowKeyboard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  InputBarFollowKeyboard
+//
+//  Created by CY on 26/1/15.
+//  Copyright (c) 2015年 LINYC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ InputBarFollowKeyboard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  InputBarFollowKeyboard
+//
+//  Created by CY on 26/1/15.
+//  Copyright (c) 2015年 LINYC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
